0d79237cb4e1945396eb5025dac3c18d63c233c2
Saleh Shalabi <ss225bx@student.lnu.se>
2021-03-15 14:20:04 +0100

    done

--- py2.py
+++ py2.py
@@ -1,8 +1,6 @@
 "The author of this code is Saleh Shalabi with student account ss225bx and Emma Lövgren with student account el222wg "
 
 
-
-
 # Imports
 ##############################
 
@@ -16,21 +14,43 @@ import csv
 
 db = mysql.connector.connect(user = 'root', password= 'root', host='127.0.0.1') 
 
-# This worked in linux but not in windows
-# the problem we got was that it can't use a data base with capitelletters, it will be "shalabi_lövgren" and not "Shalabi_Lövgren"
-# in linux it worked without any problem
+database = "lövgren_shalabi"
 
-database = "testet"
+# Veriebels
+############################## 
 
+db1 = db.cursor()
+
+pathmembers = 'members'
+patheconomy = 'economy'
+pathleaders = 'leaders'
+pathactivity = "activity"
+pathmembINact = "memInAct"
+
+pathes =  [pathmembers, pathleaders, patheconomy, pathactivity, pathmembINact]
+
+menu = """ 
+        Menu
+
+        1. List all names of the members that participate in an activity
+        2. Sum the cost of all activities for each leader 
+        3. List all names of the members that have participated in more than (X) activities.
+        4. Select the activity name and the leader’s name for the activity with most members in it
+        5. Select the leaders that don’t leads any activity
+        6. Select the sum of Amount for all activity with name (X)
+        0. Quit!
+        
+       """
 
 
 # Functions
 ############################## 
     
 def checking_db(db1, DB):
-    # function that checks if ther is a database with the given name
+    # function that checks if there is a database with the given name
+    # else it will be created
     
-    global pathes
+    global pathes, db
     
     try:
         db1.execute(f"use {DB}")
@@ -57,14 +77,19 @@ def checking_db(db1, DB):
             
             for c in pathes:
                 add_prim_key(db1,c)
-                
 
             add_foreign(db1, pathleaders)
             add_foreign(db1, pathactivity)
             add_foreign(db1, pathmembINact)
 
             for c in pathes:
-                insert_into_table(db1, c)    
+                insert_into_table(db1, c)
+
+            # when all create and inserting is done
+            db1.execute("update activity, economy set activity.transID = economy.ID where activity.ID = economy.activityID")
+            db1.execute("alter table economy add unique(activityID) ")    
+            db.commit() # commiting the changes so all should be saved
+
     except Exception as e:
         # if there is no database with the given name, create new one
         if str(e) == (f"1049 (42000): Unknown database '{DB}'"):
@@ -81,24 +106,28 @@ def checking_db(db1, DB):
             for c in pathes:
                 add_prim_key(db1,c)
                 
-
             add_foreign(db1, pathleaders)
             add_foreign(db1, pathactivity)
             add_foreign(db1, pathmembINact)
 
             for c in pathes:
-                insert_into_table(db1, c) # insert data to the table 
+                insert_into_table(db1, c) # insert data to the table
+            
+            #when all its done  
+            db1.execute("update activity, economy set activity.transID = economy.ID where activity.ID = economy.activityID")
+            db1.execute("alter table economy add unique(activityID) ")
+            db.commit() # commiting the changes so all should be saved
 
         else:
             # if another fault print it 
             print(e)
-    
+        
     print("")
     print("===========")
     print("")
 
 def create_tables(db1 ,filen):
-    # function that creat tables in database of the given csv file
+    # function that create tables in database of the given csv files
 
 
     try:
@@ -125,20 +154,12 @@ def create_tables(db1 ,filen):
         print(e)
         exit()
     
-    
-    
     x = 0
    
     for c in too:
-
-
         #  in this case the columns values will be assumed to strings no longer than 100 char
         # it is  not optimal 
         # but it works here for the given task
-
-
-
-
         if x == 0:
             # if its the first column to add to the table
             # then ther is no table yet 
@@ -174,20 +195,16 @@ def create_tables(db1 ,filen):
     print("")
 
 def insert_into_table(db1, table):
-
     # function to add the data from file to table in database
      
-
     with open(f"{table}.csv", newline='') as csvfile:
         read = csv.reader(csvfile, delimiter=',')
         x = 0
-
         lista= []
         for row in read:
             values = []
             if x == 0:
                 # if its the first row from file then its the columns name add it to a diffrent list
-
                 for c in row:
                     lista.append(c)
                 x+=1
@@ -202,11 +219,7 @@ def insert_into_table(db1, table):
                         values.append(None)
                     else:
                         values.append(str(c))
-                
-
-            try: 
-
-
+            try:
                 ################
                # this script will do same thing as following 
                # x = "%s, %s, %s, %s, %s, %s, %s, %s" 
@@ -217,15 +230,11 @@ def insert_into_table(db1, table):
                 x = x[::-1]
                 ###############
 
-
                 # here it will be as following
                 # y = f"insert into {table} values(%s, %s, %s, %s, %s, %s, %s, %s)"
                 y = f"insert into {table} values({x})"
-
                 values = tuple(values) # makes it easyer to execute query so no need to loop and add this >>>> ( )
-
                 db1.execute(y, values) # index 0 in values will take the first %s and index 1 will take the seconde and so on 
-
                 print("Adding new data... ", end = " ")
             
             except mysql.connector.Error as err:
@@ -238,7 +247,9 @@ def insert_into_table(db1, table):
     print("")
         
 def add_prim_key(db1, table):
-    ## show the columns in the table
+
+    ## this function will make the ID attribut as primary key in all tabels
+    # but when its the table "memInAct" it will add all attributs as primary key
     pathmembINact = "memInAct"
    
     
@@ -252,10 +263,11 @@ def add_prim_key(db1, table):
     print(f"")
 
 def add_foreign(db1,table):
-    
+    # this function will add foreign key for the tables that needs it 
+    # and its hardcoded  
+
     pathleaders = 'leaders'
-    pathactivity = "activity"
-    
+    pathactivity = "activity"  
 
     print(f"choose the foreign key for table {table}")
     db1.execute(f"show columns from {table}")
@@ -300,8 +312,7 @@ def run(db1, menu):
         choose = input("Choose from menu:...    ")
         print()
         
-        if choose == "1":
-            # show all planets in database 
+        if choose == "1": 
             print()
             print("the members")
             print("================")
@@ -312,24 +323,21 @@ def run(db1, menu):
                         "WHERE members.ID = memInAct.memberID "
                         "GROUP BY ID") 
 
-            # i colud just loop and print it all 
+            # I colud just loop and print it all 
             # but the following script is only to be printed in a nicer way           
             
             print("firstName", end =" "*(20-len("firstName")))
             print("lastName", end =" "*(20-len("lastName")))
             print("ID", end =" "*(20-len("ID")))
             print()
-            nr = 0
+            nr = 0 # a counter
             for c in db1:
                 print()    
                 print(c[0], end = " "*(20-len(c[0])))
                 print(c[1], end = " "*(20-len(c[1]))) 
                 print(c[2],end = "") 
                 nr+=1
-            
-                
-                
-            
+                  
             print()
             print()
             print("Total number of members that have at least one activity is : ", nr)
@@ -338,30 +346,24 @@ def run(db1, menu):
 
             input("Press for return to menu ...     ")
             print("=================================")
-            
-
-
 
         elif choose == "2":
+
             print()
             print("Sum the cost of all activities for each leader")
             print("================")
             print()
     
-        
             db1.execute("SELECT leader as ID, firstName, lastName, sum(cost) as sumOfCost "
                         "FROM economy, members, activity "
                         "WHERE economy.ID = activity.transID AND activity.leader = members.ID "
                         "GROUP BY  activity.leader")
-            
             
             print("ID", end =" "*(20-len("ID")))
             print("firstName", end =" "*(20-len("firstName")))
             print("lastName", end =" "*(20-len("lastName")))
             print("sumOfCost", end =" "*(20-len("sumOfCost")))
             print()
-
-
 
             for c in db1:
                 print()    
@@ -379,31 +381,22 @@ def run(db1, menu):
 
         elif choose == "3":
 
-            print("")
-            
+            print("")    
             # check for right input from user
-
             while True:
                 menAct = (input("How meny activitys ? ... "))
                 print("")
-
                 try:
                     menAct = int(menAct)
                     if menAct <= 0:
                         print("Please enter a positive number")
                         print()
                         continue
-
-                    else: break  
-                    
+                    else: break     
                 except Exception: 
                     print("Please enter right values!")
-                    print()     
-                    
-
-
-            # it will shows only species with hight greater than the given input
-            # is it was descriped in the task
+                    print()
+                
             db1.execute("SELECT firstName, lastName, activitys "
                         "FROM members " 
                         "JOIN (SELECT COUNT(activityID) AS activitys, memberID "
@@ -411,29 +404,21 @@ def run(db1, menu):
                         "GROUP BY memberID) AS particByMem ON particByMem.memberID = ID "
                         f"WHERE activitys > {menAct}")
             
-            
-
-            
             print("firstName", end =" "*(20-len("firstName")))
             print("lastName", end =" "*(20-len("lastName")))
             print("activitys", end =" "*(20-len("activitys")))
             print()
-
 
             for c in db1:
                 print()    
                 print(c[0], end = " "*(20-len(c[0])))
                 print(c[1], end = " "*(20-len(c[1]))) 
                 print(c[2], end = "")
-                
-                
-                    
+                   
             print()
             print()      
             input("Press for return to menu ... ")
             print("=================================")
-
-
 
         elif choose == "4":
 
@@ -442,13 +427,14 @@ def run(db1, menu):
             print("================")
             print()
     
-            try:        
+            try:       # check if the view allrady have been created else pass
                 db1.execute("CREATE VIEW memParAct AS SELECT COUNT(memberID) AS membersParAct, activityID, name, leader "
                             "FROM memInAct "
                             "JOIN activity ON activity.ID = activityID "
                             "GROUP BY activityID")
             except Exception:
                 pass
+                # the main query
             db1.execute("SELECT membersParAct, activityID, name, leader AS leaderID, firstName, lastName "
                         "FROM memParAct "
                         "JOIN members ON members.ID = leader "
@@ -461,8 +447,6 @@ def run(db1, menu):
             print("firstName", end =" "*(20-len("firstName")))
             print("lastName", end =" "*(20-len("lastName")))
             print()
-            
-
 
             for c in db1:
                 print()
@@ -471,16 +455,12 @@ def run(db1, menu):
                 print(c[2], end = " "*(20-len(str(c[2])))) 
                 print(c[3], end = " "*(20-len(str(c[3]))))
                 print(c[4], end = " "*(20-len(str(c[4])))) 
-                print(c[5], end = " "*(20-len(str(c[5])))) 
-                
-
-                
+                print(c[5], end = " "*(20-len(str(c[5]))))     
                 
             print()         
             print()      
             input("Press for return to menu ... ")
             print("=================================")
-
         
         elif choose == "5":
 
@@ -488,13 +468,11 @@ def run(db1, menu):
             print("Leaders not leading any activity")
             print("================")
             print()
-            
+
             db1.execute("SELECT firstName, lastName, members.ID AS ID "
                         "FROM members "
                         "JOIN (SELECT * FROM leaders WHERE ID  NOT IN (SELECT leader FROM activity)) AS leadersNoLead ON leadersNoLead.ID = members.ID") 
-
-            # i colud just loop and print it all 
-            # but the following script is only to be printed in a nicer way           
+       
             
             print("firstName", end =" "*(20-len("firstName")))
             print("lastName", end =" "*(20-len("lastName")))
@@ -507,11 +485,7 @@ def run(db1, menu):
                 print(c[0], end = " "*(20-len(c[0])))
                 print(c[1], end = " "*(20-len(c[1]))) 
                 print(c[2],end = "")
-                nr+=1
-
-            
-                
-                
+                nr+=1   
             
             print()
             print()
@@ -521,6 +495,7 @@ def run(db1, menu):
 
             input("Press for return to menu ...     ")
             print("=================================")
+
         elif choose == "6":
             
             print("")
@@ -542,22 +517,14 @@ def run(db1, menu):
                             "WHERE economy.ID = activity.transID "
                             "GROUP BY activity.name")
                 
-                
-
-                
                     print("activityName", end =" "*(20-len("activityName")))
                     print("totalCost", end =" "*(20-len("totalCost")))
                     print()
-
-                    
-
 
                     for c in db1:
                         print()    
                         print(c[0], end = " "*(20-len(str(c[0]))))
                         print(c[1], end = " "*(20-len(str(c[1])))) 
-                        
-                        
                             
                     print()
                     print()      
@@ -568,33 +535,20 @@ def run(db1, menu):
                 elif choose2 == "2":
                     actname = (input("What activity? ... "))
                     print("")
-
-                            
-                            
-
-
-                    # it will shows only species with hight greater than the given input
-                    # is it was descriped in the task
+       
                     db1.execute("SELECT name AS activityName, SUM(cost) AS totalCost "
                                 "FROM activity, economy "
                                 f"WHERE economy.ID = activity.transID AND activity.name = '{actname}' ")
                     
-                    
-
-                    
                     print("activityName", end =" "*(20-len("activityName")))
                     print("totalCost", end =" "*(20-len("totalCost")))
                     print()
-
-
 
                     for c in db1:
                         print()    
                         print(c[0], end = " "*(20-len(str(c[0]))))
                         print(c[1], end = " "*(20-len(str(c[1])))) 
                         
-                        
-                            
                     print()
                     print()      
                     input("Press for return to menu ... ")
@@ -605,44 +559,12 @@ def run(db1, menu):
                 else: continue
         else: continue
 
-
-
-# Veriebels
-############################## 
-
-pathmembers = 'members'
-patheconomy = 'economy'
-pathleaders = 'leaders'
-pathactivity = "activity"
-pathmembINact = "memInAct"
-
-pathes =  [pathmembers, pathleaders, patheconomy, pathactivity, pathmembINact]
-
-menu = """ 
-        Menu
-
-        1. List all names of the members that participate in an activity
-        2. Sum the cost of all activities for each leader 
-        3. List all names of the members that have participated in more than (X) activities.
-        4. Select the activity name and the leader’s name for the activity with most members in it
-        5. Select the leaders that don’t leads any activity
-        6. Select the sum of Amount for all activity with name (X)
-        0. Quit!
-        
-       """
-db1 = db.cursor()
-
-
-
 # The Program
 ############################## 
 
 checking_db(db1, database)
-db1.execute("update activity, economy set activity.transID = economy.ID where activity.ID = economy.activityID")
-db1.execute("alter table economy add unique(activityID) ")
-db.commit()
-
 run(db1,menu)
+
 # Exit statment
 ############################## 
 
